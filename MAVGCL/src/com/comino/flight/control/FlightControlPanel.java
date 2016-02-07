9427ff9e16d77a5be3ac0e0967c8fbe423666026
ecmnet <ecm@gmx.de>
2016-02-07 13:42:32 +0100

    ControlPanel added

---
+++ MAVGCL/src/com/comino/flight/control/FlightControlPanel.java
@@ -0,0 +1,53 @@
+package com.comino.flight.control;
+
+import java.io.IOException;
+
+import com.comino.flight.widgets.analysiscontrol.AnalysisControlWidget;
+import com.comino.flight.widgets.battery.BatteryWidget;
+import com.comino.flight.widgets.details.DetailsWidget;
+import com.comino.flight.widgets.status.StatusWidget;
+import com.comino.mav.control.IMAVController;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+public class FlightControlPanel extends Pane  {
+
+	@FXML
+	private StatusWidget status;
+
+	@FXML
+	private AnalysisControlWidget xyanalysiscontrol;
+
+	@FXML
+	private BatteryWidget battery;
+
+	@FXML
+	private DetailsWidget details;
+
+	public FlightControlPanel() {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FlightControlPanel.fxml"));
+		fxmlLoader.setRoot(this);
+		fxmlLoader.setController(this);
+		try {
+			fxmlLoader.load();
+		} catch (IOException exception) {
+
+			throw new RuntimeException(exception);
+		}
+
+	}
+
+	public AnalysisControlWidget getRecordControl() {
+		return xyanalysiscontrol;
+	}
+
+	public void setup(IMAVController control) {
+		xyanalysiscontrol.setup(control);
+		status.setup(control);
+		battery.setup(control);
+		details.setup(control);
+	}
+
+}
