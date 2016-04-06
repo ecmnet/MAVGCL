6ea37ebdc17b976e979b52e274a679654e24f2a4
ecmnet <ecm@gmx.de>
2016-04-06 14:27:13 +0200

    Experimental setup

---
+++ MAVGCL/src/com/comino/flight/experimental/OffboardSimulationUpdater.java
@@ -0,0 +1,61 @@
+package com.comino.flight.experimental;
+
+import org.mavlink.messages.MAV_CMD;
+import org.mavlink.messages.MAV_MODE_FLAG;
+import org.mavlink.messages.lquac.msg_set_position_target_local_ned;
+import org.mavlink.messages.lquac.msg_vision_position_estimate;
+
+import com.comino.mav.control.IMAVController;
+import com.comino.mav.mavlink.MAV_CUST_MODE;
+
+public class OffboardSimulationUpdater implements Runnable {
+
+	private IMAVController control = null;
+	private boolean isRunning = false;
+
+	public OffboardSimulationUpdater(IMAVController control) {
+		this.control = control;
+	}
+
+	public void start() {
+		System.out.println("Offboard updater started");
+		control.sendMAVLinkCmd(MAV_CMD.MAV_CMD_DO_SET_MODE,
+				MAV_MODE_FLAG.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED | MAV_MODE_FLAG.MAV_MODE_FLAG_SAFETY_ARMED,
+				MAV_CUST_MODE.PX4_CUSTOM_MAIN_MODE_OFFBOARD, 0 );
+		isRunning = true;
+		new Thread(this).start();
+
+	}
+
+	public void stop() {
+		System.out.println("Offboard updater stopped");
+        isRunning = false;
+	}
+
+
+	@Override
+	public void run() {
+
+		while(isRunning) {
+
+			msg_set_position_target_local_ned cmd = new msg_set_position_target_local_ned(255,1);
+			cmd.target_component = 1;
+			cmd.target_system = 1;
+			cmd.x = 1;
+			cmd.y = 1;
+			cmd.z = -2;
+			control.sendMAVLinkMessage(cmd);
+
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) { }
+
+
+
+		}
+
+
+
+	}
+
+}
---
+++ MAVGCL/src/com/comino/flight/experimental/VisionPositionSimulationUpdater.java
@@ -0,0 +1,50 @@
+package com.comino.flight.experimental;
+
+import org.mavlink.messages.lquac.msg_vision_position_estimate;
+
+import com.comino.mav.control.IMAVController;
+
+public class VisionPositionSimulationUpdater implements Runnable {
+
+	private IMAVController control = null;
+	private boolean isRunning = false;
+
+	public VisionPositionSimulationUpdater(IMAVController control) {
+		this.control = control;
+	}
+
+	public void start() {
+		System.out.println("Vision updater started");
+		isRunning = true;
+		new Thread(this).start();
+
+	}
+
+	public void stop() {
+		System.out.println("Vision updater stopped");
+        isRunning = false;
+	}
+
+
+	@Override
+	public void run() {
+
+		while(isRunning) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) { }
+
+			msg_vision_position_estimate cmd = new msg_vision_position_estimate(255,1);
+			cmd.x = (float)Math.random();
+			cmd.y = (float)Math.random();
+			cmd.z = -5;
+			control.sendMAVLinkMessage(cmd);
+
+
+		}
+
+
+
+	}
+
+}
