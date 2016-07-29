f4ddaa2a4c35e207349facfdd4807984da4a08c8
ecmnet <ecm@gmx.de>
2016-07-29 07:55:18 +0200

    StartUp: Keep focus

--- MAVGCL/src/com/comino/flight/MainApp.java
+++ MAVGCL/src/com/comino/flight/MainApp.java
@@ -225,6 +225,7 @@ public class MainApp extends Application  {
 
 			primaryStage.setScene(scene);
 			primaryStage.show();
+		    primaryStage.setAlwaysOnTop(true);
 
 
 		} catch (IOException e) {
