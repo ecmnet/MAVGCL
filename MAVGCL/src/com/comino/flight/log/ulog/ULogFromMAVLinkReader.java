46cd1cb199ac6e88f7606dc711bf341ef2694cc2
ecmnet <ecm@gmx.de>
2016-11-10 15:39:34 +0100

    Some housekeeping

--- MAVGCL/src/com/comino/flight/log/ulog/ULogFromMAVLinkReader.java
+++ MAVGCL/src/com/comino/flight/log/ulog/ULogFromMAVLinkReader.java
@@ -1,3 +1,36 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2016 Eike Mansfeld ecm@gmx.de. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
 package com.comino.flight.log.ulog;
 
 import java.util.Map;
@@ -14,8 +47,6 @@ import com.comino.jmavlib.extensions.UlogMAVLinkParser;
 import com.comino.mav.control.IMAVController;
 import com.comino.msp.log.MSPLogger;
 import com.comino.msp.main.control.listener.IMAVLinkListener;
-
-
 
 
 public class ULogFromMAVLinkReader implements IMAVLinkListener {
@@ -124,38 +155,33 @@ public class ULogFromMAVLinkReader implements IMAVLinkListener {
 					control.sendMAVLinkCmd(MAV_CMD.MAV_CMD_LOGGING_STOP);
 					state=STATE_HEADER_IDLE;
 				}
-				//else
 				parser.addToBuffer(log.data, log.length,log.first_message_offset, true);
 				parser.parseData();
 				package_processed++;
 			}
-
 		}
-
 	}
 
 	//  helpers for dev
-	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
-	public static String bytesToHex(byte[] bytes, int len) {
-		char[] hexChars = new char[len * 2];
-		for ( int j = 0; j <len; j++ ) {
-			int v = bytes[j] & 0xFF;
-			hexChars[j * 2] = hexArray[v >>> 4];
-			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-		}
-		return new String(hexChars);
-	}
-
-	public static String intsToHex(int[] bytes, int len) {
-		char[] hexChars = new char[len * 2];
-		for ( int j = 0; j <len; j++ ) {
-			int v = bytes[j] & 0xFF;
-			hexChars[j * 2] = hexArray[v >>> 4];
-			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-		}
-		return new String(hexChars);
-	}
-
-
+//	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+//	public static String bytesToHex(byte[] bytes, int len) {
+//		char[] hexChars = new char[len * 2];
+//		for ( int j = 0; j <len; j++ ) {
+//			int v = bytes[j] & 0xFF;
+//			hexChars[j * 2] = hexArray[v >>> 4];
+//			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+//		}
+//		return new String(hexChars);
+//	}
+//
+//	public static String intsToHex(int[] bytes, int len) {
+//		char[] hexChars = new char[len * 2];
+//		for ( int j = 0; j <len; j++ ) {
+//			int v = bytes[j] & 0xFF;
+//			hexChars[j * 2] = hexArray[v >>> 4];
+//			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+//		}
+//		return new String(hexChars);
+//	}
 
 }
