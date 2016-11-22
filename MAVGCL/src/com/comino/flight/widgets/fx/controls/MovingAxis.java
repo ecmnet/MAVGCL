57da6208124df38f46956a569a57c4fb320ddd25
ecmnet <ecm@gmx.de>
2016-11-22 21:34:44 +0100

    Memory usage fix

--- MAVGCL/src/com/comino/flight/widgets/fx/controls/MovingAxis.java
+++ MAVGCL/src/com/comino/flight/widgets/fx/controls/MovingAxis.java
@@ -43,53 +43,55 @@ import javafx.scene.chart.ValueAxis;
 public class MovingAxis extends ValueAxis<Number> {
 
 	private DecimalFormat fo = new DecimalFormat("#0");
+	private List<Number> mticks = new ArrayList<Number>();
+	private List<Number> vticks = new ArrayList<Number>();
 
-    private SimpleObjectProperty<Double> tickUnitProperty = new SimpleObjectProperty<Double>(
-            5d);
+	private SimpleObjectProperty<Double> tickUnitProperty = new SimpleObjectProperty<Double>(
+			5d);
 
-    @Override
-    protected List<Number> calculateMinorTickMarks() {
-        List<Number> ticks = new ArrayList<Number>();
-        double tickUnit = tickUnitProperty.get() / getMinorTickCount();
-        double start = Math.floor(getLowerBound() / tickUnit) * tickUnit;
-        for (double value = start; value <= getUpperBound(); value += tickUnit) {
-            ticks.add(value);
-        }
-        return ticks;
-    }
+	@Override
+	protected List<Number> calculateMinorTickMarks() {
+		mticks.clear();
+		double tickUnit = tickUnitProperty.get() / getMinorTickCount();
+		double start = Math.floor(getLowerBound() / tickUnit) * tickUnit;
+		for (double value = start; value <= getUpperBound(); value += tickUnit) {
+			mticks.add(value);
+		}
+		return mticks;
+	}
 
-    @Override
-    protected List<Number> calculateTickValues(double arg0, Object arg1) {
-        List<Number> ticks = new ArrayList<Number>();
-        double tickUnit = tickUnitProperty.get();
-        double start = Math.floor(getLowerBound() / tickUnit) * tickUnit;
-        for (double value = start; value <= getUpperBound(); value += tickUnit) {
-            ticks.add(value);
-        }
-        return ticks;
-    }
+	@Override
+	protected List<Number> calculateTickValues(double arg0, Object arg1) {
+		vticks.clear();
+		double tickUnit = tickUnitProperty.get();
+		double start = Math.floor(getLowerBound() / tickUnit) * tickUnit;
+		for (double value = start; value <= getUpperBound(); value += tickUnit) {
+			vticks.add(value);
+		}
+		return vticks;
+	}
 
-    @Override
-    protected Object getRange() {
-       return null;
-    }
+	@Override
+	protected Object getRange() {
+		return null;
+	}
 
-    @Override
-    protected String getTickMarkLabel(Number label) {
-        return fo.format(label.doubleValue());
-    }
+	@Override
+	protected String getTickMarkLabel(Number label) {
+		return fo.format(label.doubleValue());
+	}
 
-    @Override
-    protected void setRange(Object range, boolean arg1) {
-        // not sure how this is used??
-    }
+	@Override
+	protected void setRange(Object range, boolean arg1) {
+		// not sure how this is used??
+	}
 
-    public SimpleObjectProperty<Double> getTickUnitProperty() {
-        return tickUnitProperty;
-    }
+	public SimpleObjectProperty<Double> getTickUnitProperty() {
+		return tickUnitProperty;
+	}
 
-    public void setTickUnit(double tickUnit) {
-        tickUnitProperty.set(tickUnit);
-    }
+	public void setTickUnit(double tickUnit) {
+		tickUnitProperty.set(tickUnit);
+	}
 
 }
