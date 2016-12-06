893f27527805cf50bb9210a1d4de702c8ad41925
ecmnet <ecm@gmx.de>
2016-12-06 22:44:42 +0100

    Set CollectorCycle to 25ms

--- MAVGCL/src/com/comino/flight/model/service/AnalysisModelService.java
+++ MAVGCL/src/com/comino/flight/model/service/AnalysisModelService.java
@@ -75,7 +75,7 @@ public class AnalysisModelService implements IMAVLinkListener {
 	private int     mode = 0;
 
 	private  int  totalTime_sec = 30;
-	private  int collector_interval_us = 50000;
+	private  int collector_interval_us = 25000;
 
 	public static AnalysisModelService getInstance(IMAVController control) {
 		if(instance==null)
