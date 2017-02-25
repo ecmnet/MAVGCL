b4e0e69c944a40147a3a8450daa0be72b676fd55
ecmnet <ecm@gmx.de>
2017-02-25 13:01:53 +0100

    CleanUp

--- MAVGCL/src/com/comino/flight/base/UBXRTCM3Base.java
+++ MAVGCL/src/com/comino/flight/base/UBXRTCM3Base.java
@@ -118,7 +118,6 @@ public class UBXRTCM3Base {
 			public void getSurveyIn(float time_svin, boolean is_svin, boolean is_valid, float meanacc) {
 				svin.set(is_svin);
 				mean_acc = meanacc;
-
 				if((time_svin % 30) == 0)
 					MSPLogger.getInstance().writeLocalMsg("[mgc] Survey-In: "+meanacc+"m ["+(int)base.numsat+"]", MAV_SEVERITY.MAV_SEVERITY_NOTICE);
 			}
