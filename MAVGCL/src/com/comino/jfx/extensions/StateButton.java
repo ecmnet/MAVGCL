cbecb38901947fb998191fd88e1d1ba8666e6dd4
ecmnet <ecm@gmx.de>
2017-09-16 09:07:04 +0200

    StateButton: For feedback based toggles

---
+++ MAVGCL/src/com/comino/jfx/extensions/StateButton.java
@@ -0,0 +1,23 @@
+package com.comino.jfx.extensions;
+
+import javafx.application.Platform;
+import javafx.scene.control.Button;
+
+public class StateButton extends Button {
+
+	private boolean state = false;
+
+	public void setState(boolean state) {
+		this.state = state;
+		Platform.runLater(() -> {
+			if(state)
+				setStyle("-fx-background-color: steelblue");
+			else
+				setStyle("-fx-background-color: #606060");
+		});
+	}
+
+	public boolean getState() {
+		return state;
+	}
+}
