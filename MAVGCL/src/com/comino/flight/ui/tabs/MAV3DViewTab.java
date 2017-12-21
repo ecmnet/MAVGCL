593cf2d3292fd6c5c319c1c93d4666e1ddab714b
ecmnet <ecm@gmx.de>
2017-12-21 11:50:59 +0100

    3d World added

--- MAVGCL/src/com/comino/flight/ui/tabs/MAV3DViewTab.java
+++ MAVGCL/src/com/comino/flight/ui/tabs/MAV3DViewTab.java
@@ -38,19 +38,41 @@ import com.comino.flight.file.KeyFigurePreset;
 import com.comino.flight.ui.widgets.panel.ChartControlWidget;
 import com.comino.flight.ui.widgets.panel.IChartControl;
 import com.comino.mav.control.IMAVController;
+import com.comino.mav3d.Xform;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.FloatProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.fxml.FXML;
+import javafx.scene.DepthTest;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.SubScene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
 
 
 
 public class MAV3DViewTab extends BorderPane implements IChartControl {
 
 
-
+	private final Group root = new Group();
+    private final Xform world = new Xform();
+    private final PerspectiveCamera camera = new PerspectiveCamera(true);
+    private final Xform cameraXform = new Xform();
+    private final Xform cameraXform2 = new Xform();
+    private final Xform cameraXform3 = new Xform();
+    private final Xform axisGroup = new Xform();
+	private SubScene subScene;
+    private static final double CAMERA_INITIAL_DISTANCE = -450;
+    private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
+    private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
+    private static final double CAMERA_NEAR_CLIP = 0.1;
+    private static final double CAMERA_FAR_CLIP = 10000.0;
+    private static final double AXIS_LENGTH = 250.0;
 
 	public MAV3DViewTab() {
 		FXMLLoadHelper.load(this, "MAV3DViewTab.fxml");
@@ -61,6 +83,16 @@ public class MAV3DViewTab extends BorderPane implements IChartControl {
 
 	@FXML
 	private void initialize() {
+
+		buildCamera();
+        buildAxes();
+        root.getChildren().add(world);
+        root.setDepthTest(DepthTest.ENABLE);
+        subScene = new SubScene(root,this.getPrefWidth(),this.getPrefHeight(),true,javafx.scene.SceneAntialiasing.BALANCED);
+
+        this.setCenter(subScene);
+        subScene.setCamera(camera);
+        this.setBottom(new Label("Test"));
 
 
 	}
@@ -111,6 +143,48 @@ public class MAV3DViewTab extends BorderPane implements IChartControl {
 		// TODO Auto-generated method stub
 
 	}
+
+	private void buildCamera() {
+        root.getChildren().add(cameraXform);
+        cameraXform.getChildren().add(cameraXform2);
+        cameraXform2.getChildren().add(cameraXform3);
+        cameraXform3.getChildren().add(camera);
+        cameraXform3.setRotateZ(180.0);
+
+        camera.setNearClip(CAMERA_NEAR_CLIP);
+        camera.setFarClip(CAMERA_FAR_CLIP);
+        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+        cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+        camera.setVisible(true);
+    }
+
+	private void buildAxes() {
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+
+        final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
+        final Box yAxis = new Box(1, AXIS_LENGTH, 1);
+        final Box zAxis = new Box(1, 1, AXIS_LENGTH);
+
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+
+        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+        axisGroup.setVisible(true);
+        world.getChildren().addAll(axisGroup);
+    }
+
 
 
 }
