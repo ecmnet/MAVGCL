7e5422e5deeb6ad57eced5b51f51de0e9bc88441
ecmnet <ecm@gmx.de>
2017-12-21 20:48:08 +0100

    Zooming, vehicle display (WIP)

--- MAVGCL/src/com/comino/flight/ui/tabs/MAV3DViewTab.java
+++ MAVGCL/src/com/comino/flight/ui/tabs/MAV3DViewTab.java
@@ -55,7 +55,7 @@ import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.SubScene;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
@@ -64,41 +64,41 @@ import javafx.util.Duration;
 
 
 
-public class MAV3DViewTab extends Pane implements IChartControl {
+public class MAV3DViewTab extends StackPane implements IChartControl {
 
 	private Timeline task = null;
 
 	private final Group root = new Group();
-    private final Xform world = new Xform();
-    private final PerspectiveCamera camera = new PerspectiveCamera(true);
-    private final Xform cameraXform = new Xform();
-    private final Xform cameraXform2 = new Xform();
-    private final Xform cameraXform3 = new Xform();
-    private final Xform axisGroup = new Xform();
-    private final Xform mapGroup = new Xform();
+	private final Xform world = new Xform();
+	private final PerspectiveCamera camera = new PerspectiveCamera(true);
+	private final Xform cameraXform = new Xform();
+	private final Xform cameraXform2 = new Xform();
+	private final Xform cameraXform3 = new Xform();
+	private final Xform axisGroup = new Xform();
+	private final Xform mapGroup = new Xform();
 	private SubScene subScene;
 
 	private Sphere vehicle = null;
 
-    private static final double CAMERA_INITIAL_DISTANCE = -450;
-    private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
-    private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
-    private static final double CAMERA_NEAR_CLIP = 0.1;
-    private static final double CAMERA_FAR_CLIP = 10000.0;
-    private static final double AXIS_LENGTH = 250.0;
+	private static final double CAMERA_INITIAL_DISTANCE = -450;
+	private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
+	private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
+	private static final double CAMERA_NEAR_CLIP = 0.1;
+	private static final double CAMERA_FAR_CLIP = 10000.0;
+	private static final double AXIS_LENGTH = 250.0;
 
-    private static final double CONTROL_MULTIPLIER = 0.1;
-    private static final double SHIFT_MULTIPLIER = 10.0;
-    private static final double MOUSE_SPEED = 0.1;
-    private static final double ROTATION_SPEED = 2.0;
-    private static final double TRACK_SPEED = 0.3;
+	private static final double CONTROL_MULTIPLIER = 0.1;
+	private static final double SHIFT_MULTIPLIER = 10.0;
+	private static final double MOUSE_SPEED = 0.1;
+	private static final double ROTATION_SPEED = 2.0;
+	private static final double TRACK_SPEED = 0.3;
 
-    double mousePosX;
-    double mousePosY;
-    double mouseOldX;
-    double mouseOldY;
-    double mouseDeltaX;
-    double mouseDeltaY;
+	double mousePosX;
+	double mousePosY;
+	double mouseOldX;
+	double mouseOldY;
+	double mouseDeltaX;
+	double mouseDeltaY;
 
 	private DataModel model;
 
@@ -106,19 +106,18 @@ public class MAV3DViewTab extends Pane implements IChartControl {
 		FXMLLoadHelper.load(this, "MAV3DViewTab.fxml");
 		task = new Timeline(new KeyFrame(Duration.millis(50), ae -> {
 			Platform.runLater(() -> {
-				updateMap(true);
+				updatePosition();
 			});
 		} ) );
 		task.setCycleCount(Timeline.INDEFINITE);
 		task.play();
-
 	}
 
 
-	private void updateMap(boolean b) {
-         vehicle.setTranslateX(model.state.l_y*40);
-         vehicle.setTranslateY(-model.state.l_z*40);
-         vehicle.setTranslateZ(model.state.l_x*40);
+	private void updatePosition() {
+		vehicle.setTranslateX(model.state.l_y*40);
+		vehicle.setTranslateY(-model.state.l_z*40);
+		vehicle.setTranslateZ(model.state.l_x*40);
 	}
 
 
@@ -126,29 +125,29 @@ public class MAV3DViewTab extends Pane implements IChartControl {
 	private void initialize() {
 
 		buildCamera();
-        buildAxes();
-        root.getChildren().add(world);
-        root.setDepthTest(DepthTest.ENABLE);
+		handleMouse(this);
 
-        subScene = new SubScene(root,0,0,true,javafx.scene.SceneAntialiasing.BALANCED);
-        subScene.setCamera(camera);
-        subScene.widthProperty().bind(this.widthProperty().subtract(20));
-        subScene.heightProperty().bind(this.heightProperty().subtract(20));
-        subScene.setLayoutX(10);  subScene.setLayoutY(10);
-        this.getChildren().add(subScene);
+		root.getChildren().add(world);
+		root.setDepthTest(DepthTest.ENABLE);
 
-        handleMouse(subScene, this);
+		subScene = new SubScene(root,0,0,true,javafx.scene.SceneAntialiasing.BALANCED);
+		subScene.setCamera(camera);
+		subScene.widthProperty().bind(this.widthProperty().subtract(20));
+		subScene.heightProperty().bind(this.heightProperty().subtract(20));
+		subScene.setLayoutX(10);  subScene.setLayoutY(10);
 
-        buildMap();
+		this.getChildren().add(subScene);
 
-        vehicle = buildVehicle();
+		buildMap();
+		buildAxes();
+		vehicle = buildVehicle();
 
 
 	}
 
 
 	public MAV3DViewTab setup(ChartControlWidget recordControl, IMAVController control) {
-        this.model = control.getCurrentModel();
+		this.model = control.getCurrentModel();
 
 		return this;
 	}
@@ -194,19 +193,19 @@ public class MAV3DViewTab extends Pane implements IChartControl {
 	}
 
 	private void buildCamera() {
-        root.getChildren().add(cameraXform);
-        cameraXform.getChildren().add(cameraXform2);
-        cameraXform2.getChildren().add(cameraXform3);
-        cameraXform3.getChildren().add(camera);
-        cameraXform3.setRotateZ(180.0);
+		root.getChildren().add(cameraXform);
+		cameraXform.getChildren().add(cameraXform2);
+		cameraXform2.getChildren().add(cameraXform3);
+		cameraXform3.getChildren().add(camera);
+		cameraXform3.setRotateZ(180.0);
 
-        camera.setNearClip(CAMERA_NEAR_CLIP);
-        camera.setFarClip(CAMERA_FAR_CLIP);
-        camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
-        cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
-        cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
-        camera.setVisible(true);
-    }
+		camera.setNearClip(CAMERA_NEAR_CLIP);
+		camera.setFarClip(CAMERA_FAR_CLIP);
+		camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+		cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+		cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+		camera.setVisible(true);
+	}
 
 	private Sphere buildVehicle() {
 		final PhongMaterial greenMaterial = new PhongMaterial();
@@ -227,93 +226,83 @@ public class MAV3DViewTab extends Pane implements IChartControl {
 		grayMaterial.setSpecularColor(Color.GRAY);
 
 
-        for(int i=0;i<250;i=i+5) {
-		final Box box = new Box(5, 5, 5);
-		box.setTranslateX(10+i);
-		box.setTranslateY(10+i);
-		box.setTranslateZ(Math.random()*50);
-		box.setMaterial(grayMaterial);
-		mapGroup.getChildren().addAll(box);
-        }
+		for(int i=0;i<5000;i++) {
+			final Box box = new Box(2, 2, 2);
+			box.setTranslateX(Math.random()*200-100);
+			box.setTranslateY(Math.random()*200-100);
+			box.setTranslateZ(Math.random()*200-100);
+			box.setMaterial(grayMaterial);
+			mapGroup.getChildren().addAll(box);
+		}
 		world.getChildren().addAll(mapGroup);
 	}
 
 	private void buildAxes() {
-        final PhongMaterial redMaterial = new PhongMaterial();
-        redMaterial.setDiffuseColor(Color.DARKRED);
-        redMaterial.setSpecularColor(Color.RED);
+		final PhongMaterial redMaterial = new PhongMaterial();
+		redMaterial.setDiffuseColor(Color.DARKRED);
+		redMaterial.setSpecularColor(Color.RED);
 
-        final PhongMaterial greenMaterial = new PhongMaterial();
-        greenMaterial.setDiffuseColor(Color.DARKGREEN);
-        greenMaterial.setSpecularColor(Color.GREEN);
+		final PhongMaterial greenMaterial = new PhongMaterial();
+		greenMaterial.setDiffuseColor(Color.DARKGREEN);
+		greenMaterial.setSpecularColor(Color.GREEN);
 
-        final PhongMaterial blueMaterial = new PhongMaterial();
-        blueMaterial.setDiffuseColor(Color.DARKBLUE);
-        blueMaterial.setSpecularColor(Color.BLUE);
+		final PhongMaterial blueMaterial = new PhongMaterial();
+		blueMaterial.setDiffuseColor(Color.DARKBLUE);
+		blueMaterial.setSpecularColor(Color.BLUE);
 
-        final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
-        final Box yAxis = new Box(1, AXIS_LENGTH, 1);
-        final Box zAxis = new Box(1, 1, AXIS_LENGTH);
+		final Box xAxis = new Box(AXIS_LENGTH, 1, 1);
+		final Box yAxis = new Box(1, AXIS_LENGTH, 1);
+		final Box zAxis = new Box(1, 1, AXIS_LENGTH);
 
-        xAxis.setMaterial(redMaterial);
-        yAxis.setMaterial(greenMaterial);
-        zAxis.setMaterial(blueMaterial);
+		xAxis.setMaterial(redMaterial);
+		yAxis.setMaterial(greenMaterial);
+		zAxis.setMaterial(blueMaterial);
 
-        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
-        axisGroup.setVisible(true);
-        world.getChildren().addAll(axisGroup);
-    }
+		axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+		axisGroup.setVisible(true);
+		world.getChildren().addAll(axisGroup);
+	}
 
-	private void handleMouse(SubScene subScene2, final Node root) {
+	private void handleMouse(final Node node) {
 
-        root.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent me) {
-                mousePosX = me.getSceneX();
-                mousePosY = me.getSceneY();
-                mouseOldX = me.getSceneX();
-                mouseOldY = me.getSceneY();
-            }
-        });
-        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent me) {
-                mouseOldX = mousePosX;
-                mouseOldY = mousePosY;
-                mousePosX = me.getSceneX();
-                mousePosY = me.getSceneY();
-                mouseDeltaX = (mousePosX - mouseOldX);
-                mouseDeltaY = (mousePosY - mouseOldY);
+		node.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override public void handle(MouseEvent me) {
+				mousePosX = me.getSceneX();
+				mousePosY = me.getSceneY();
+				mouseOldX = me.getSceneX();
+				mouseOldY = me.getSceneY();
+			}
+		});
+		node.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override public void handle(MouseEvent me) {
+				mouseOldX = mousePosX;
+				mouseOldY = mousePosY;
+				mousePosX = me.getSceneX();
+				mousePosY = me.getSceneY();
+				mouseDeltaX = (mousePosX - mouseOldX);
+				mouseDeltaY = (mousePosY - mouseOldY);
 
-               double modifier = 1.0;
+				double modifier = 1.0;
 
-               if (me.isControlDown()) {
-                    modifier = CONTROL_MULTIPLIER;
-                }
-                if (me.isShiftDown()) {
-                    modifier = SHIFT_MULTIPLIER;
-                }
-                if (me.isPrimaryButtonDown()) {
-                    cameraXform.ry.setAngle(cameraXform.ry.getAngle() -
-                       mouseDeltaX*MOUSE_SPEED*modifier*ROTATION_SPEED);  //
-                   cameraXform.rx.setAngle(cameraXform.rx.getAngle() +
-                       mouseDeltaY*MOUSE_SPEED*modifier*ROTATION_SPEED);  // -
-                }
-                else if (me.isSecondaryButtonDown()) {
-                    double z = camera.getTranslateZ();
-                    double newZ = z + mouseDeltaX*MOUSE_SPEED*modifier;
-                    camera.setTranslateZ(newZ);
-                }
-                else if (me.isMiddleButtonDown()) {
-                   cameraXform2.t.setX(cameraXform2.t.getX() +
-                      mouseDeltaX*MOUSE_SPEED*modifier*TRACK_SPEED);  // -
-                   cameraXform2.t.setY(cameraXform2.t.getY() +
-                      mouseDeltaY*MOUSE_SPEED*modifier*TRACK_SPEED);  // -
-                }
-           }
-       }); // setOnMouseDragged
-   } //handleMouse
+				if (me.isControlDown()) {
+					modifier = CONTROL_MULTIPLIER;
+				}
+				if (me.isShiftDown()) {
+					modifier = SHIFT_MULTIPLIER;
+				}
 
+				if (me.isPrimaryButtonDown()) {
+					cameraXform.ry.setAngle(cameraXform.ry.getAngle() -
+							mouseDeltaX*MOUSE_SPEED*modifier*ROTATION_SPEED);  //
+							cameraXform.rx.setAngle(cameraXform.rx.getAngle() +
+									mouseDeltaY*MOUSE_SPEED*modifier*ROTATION_SPEED);  // -
+				}
+			}
+		});
 
-
-
+		node.setOnZoom(event -> {
+			camera.setTranslateZ(camera.getTranslateZ() + (event.getZoomFactor()-1)*MOUSE_SPEED*3000);
+		});
+	}
 
 }
