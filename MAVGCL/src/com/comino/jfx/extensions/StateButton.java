f8b780e30d3dd690552baa35c2c2bb8a6c073c3b
ecmnet <ecm@gmx.de>
2018-02-09 21:10:06 +0100

    StateButton: Intermediate state and timeout

--- MAVGCL/src/com/comino/jfx/extensions/StateButton.java
+++ MAVGCL/src/com/comino/jfx/extensions/StateButton.java
@@ -1,11 +1,29 @@
 package com.comino.jfx.extensions;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.util.Duration;
 
 public class StateButton extends Button {
 
-	private boolean state = false;
+	private boolean state 	= false;
+
+	public StateButton() {
+
+		Timeline timeline = new Timeline(new KeyFrame(
+				Duration.millis(1500),
+				ae ->  { setState(state); } ));
+
+		this.addEventHandler(ActionEvent.ACTION, event -> {
+			timeline.playFromStart();
+			Platform.runLater(() -> {
+				setStyle("-fx-background-color: #006C6CCC");
+			});
+		});
+	}
 
 	public void setState(boolean state) {
 		this.state = state;
@@ -20,4 +38,6 @@ public class StateButton extends Button {
 	public boolean getState() {
 		return state;
 	}
+
+
 }
