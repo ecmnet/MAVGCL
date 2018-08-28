23da91ebe4a866e989040eaa3f2e39d1ca7939af
ecmnet <ecm@gmx.de>
2018-08-28 22:12:59 +0200

    Don't check for GPOS but LPOS availability

--- MAVGCL/src/com/comino/flight/ui/widgets/panel/CommanderWidget.java
+++ MAVGCL/src/com/comino/flight/ui/widgets/panel/CommanderWidget.java
@@ -145,7 +145,7 @@ public class CommanderWidget extends WidgetPane  {
 				.or(state.getRCProperty())
 				.or(StateProperties.getInstance().getLandedProperty().not()));
 		takeoff_command.setOnAction((ActionEvent event)-> {
-			if(model.hud.ag!=Float.NaN && model.sys.isStatus(Status.MSP_GPOS_VALID) ) {
+			if(model.hud.ag!=Float.NaN && model.sys.isStatus(Status.MSP_LPOS_VALID) ) {
 				control.sendMAVLinkCmd(MAV_CMD.MAV_CMD_NAV_TAKEOFF, -1, 0, 0, Float.NaN, Float.NaN, Float.NaN,
 						model.hud.at);
 			}
