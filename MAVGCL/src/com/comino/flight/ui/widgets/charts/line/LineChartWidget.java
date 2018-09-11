3a83157ad6de7138510f66eedcfc6be4554abaf6
ecmnet <ecm@gmx.de>
2018-09-11 16:10:26 +0200

    Boundary check added

--- MAVGCL/src/com/comino/flight/ui/widgets/charts/line/LineChartWidget.java
+++ MAVGCL/src/com/comino/flight/ui/widgets/charts/line/LineChartWidget.java
@@ -823,7 +823,7 @@ public class LineChartWidget extends BorderPane implements IChartControl, IColle
 				setDashboardData(dashboard3,type3, current_x0_pt,current_x1_pt);
 			}
 
-			while(current_x_pt<max_x && size>0 ) {
+			while(current_x_pt<max_x && size>0 && current_x_pt< dataService.getModelList().size()) {
 
 				m = dataService.getModelList().get(current_x_pt);
 				dt_sec = current_x_pt *  dataService.getCollectorInterval_ms() / 1000f;
