a4e5fff7f78b6796512c7a27a5481a3234567862
ecmnet <ecm@gmx.de>
2018-10-12 19:03:19 +0200

    Trigger event 'recording stopped' if disconnected

--- MAVGCL/src/com/comino/flight/model/service/AnalysisModelService.java
+++ MAVGCL/src/com/comino/flight/model/service/AnalysisModelService.java
@@ -327,6 +327,7 @@ public class AnalysisModelService implements IMAVLinkListener {
 					if(ulogger.isLogging())
 						ulogger.enableLogging(false);
 					mode = STOPPED; old_mode = STOPPED;
+					state.getRecordingProperty().set(STOPPED);
 					try { 	Thread.sleep(100); 	} catch (InterruptedException e) { 	}
 					continue;
 					//		}
