96ae5584e43fe2d5eb417b762d5b7265d6bf7a9c
ecmnet <ecm@gmx.de>
2021-01-01 20:37:16 +0100

    CleanUp

--- MAVGCL/src/com/comino/flight/model/map/IMAVMap.java
+++ MAVGCL/src/com/comino/flight/model/map/IMAVMap.java
@@ -1,10 +1,14 @@
 package com.comino.flight.model.map;
 
-import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+
+import georegression.struct.point.Point3D_I32;
 
 public interface IMAVMap {
 	
-	public Map<Integer,?> getMap();
+	public void forEach(BiConsumer<Integer,Point3D_I32 > consumer);
+	public Set<Integer> keySet();
 	public void clear();
 
 }
