03f46434b89c6d8eba72c4cec869c93979577600
ecmnet <ecm@gmx.de>
2021-05-31 08:56:38 +0200

    Landing heading fix

--- MAVGCL/src/main/java/com/comino/flight/ui/widgets/panel/CommanderWidget.java
+++ MAVGCL/src/main/java/com/comino/flight/ui/widgets/panel/CommanderWidget.java
@@ -151,7 +151,7 @@ public class CommanderWidget extends ChartControlPane  {
 			control.sendMAVLinkCmd(MAV_CMD.MAV_CMD_NAV_LAND, ( cmd,result) -> {
 				if(result != MAV_RESULT.MAV_RESULT_ACCEPTED)
 					logger.writeLocalMsg("[mgc] PX4 landing rejected ("+result+")",MAV_SEVERITY.MAV_SEVERITY_WARNING);
-			}, 0, 0, 0, model.state.h );
+			}, 0, 0, 0, model.attitude.y );
 		});
 
 		hold_command.disableProperty().bind(state.getArmedProperty().not()
