d182c7f615accd2c978725be11be122e51e808d1
ecmnet <ecm@gmx.de>
2021-11-03 07:30:15 +0100

    Update resolution fixed

--- MAVGCL/src/main/java/com/comino/flight/ui/widgets/charts/xy/XYChartWidget.java
+++ MAVGCL/src/main/java/com/comino/flight/ui/widgets/charts/xy/XYChartWidget.java
@@ -674,11 +674,11 @@ public class XYChartWidget extends BorderPane implements IChartControl, ICollect
 	private void setXResolution(int frame) {
 		this.frame_secs = frame;
 		if(frame >= 200)
-			resolution_ms = 200;
-		else if(frame >= 60)
-			resolution_ms = 100;
-		else if(frame >= 30)
 			resolution_ms = 50;
+		else if(frame >= 60)
+			resolution_ms = 20;
+		else if(frame >= 30)
+			resolution_ms = 20;
 		else if(frame >= 15)
 			resolution_ms = 20;
 		else
