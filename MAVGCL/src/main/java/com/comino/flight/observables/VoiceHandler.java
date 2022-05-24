52d291d1cf3e2a3349a4fe93cd3be6435eb81ddb
ecmnet <ecm@gmx.de>
2022-05-24 13:01:30 +0200

    Cleanup

--- MAVGCL/src/main/java/com/comino/flight/observables/VoiceHandler.java
+++ MAVGCL/src/main/java/com/comino/flight/observables/VoiceHandler.java
@@ -11,15 +11,15 @@ import com.comino.mavutils.workqueue.WorkQueue;
 import com.comino.speech.VoiceTTS;
 
 public class VoiceHandler {
-	
+
 	private static VoiceHandler instance;
+	
+	private static final float BATTERY_CAPACITY_LIMT = 60.0f;
 
 	private final WorkQueue         wq = WorkQueue.getInstance();
 	private final AnalysisDataModel model;
 	private final StateProperties   properties;
 	private final VoiceTTS          voice;
-	
-	private final boolean           enabled;
 
 	public static VoiceHandler getInstance(IMAVController control) {
 		if(instance==null)
@@ -32,56 +32,47 @@ public class VoiceHandler {
 		this.model      = AnalysisModelService.getInstance().getCurrent();
 		this.properties = StateProperties.getInstance();
 		this.voice      = VoiceTTS.getInstance();
-		this.enabled    = MAVPreferences.getInstance().getBoolean("SPEECH", false);
 
-		if(enabled) {
-			control.addMAVMessageListener(msg -> {
-				if(msg.severity != MAV_SEVERITY.MAV_SEVERITY_EMERGENCY && msg.severity != MAV_SEVERITY.MAV_SEVERITY_CRITICAL)
-					return;
-				if(msg.text.contains("]"))
-					voice.talk(msg.text.substring(msg.text.indexOf(']')));
-			});
-		}
-		
+		if(!MAVPreferences.getInstance().getBoolean("SPEECH", false))
+			return;
+
+		control.addMAVMessageListener(msg -> {
+			if(msg.severity != MAV_SEVERITY.MAV_SEVERITY_EMERGENCY && msg.severity != MAV_SEVERITY.MAV_SEVERITY_CRITICAL)
+				return;
+			if(msg.text.contains("]"))
+				voice.talk(msg.text.substring(msg.text.indexOf(']')));
+		});
+
 		// Report takeoff and landed state if armed
 		properties.getLandedProperty().addListener((s,o,n) -> {
-			
+
 			if(!properties.getArmedProperty().get())
 				return;
-			
+
 			if(o.booleanValue() && !n.booleanValue()) {
-				if(enabled)
-					voice.talk("Takeoff");
+				voice.talk("Takeoff");
 			}
-			
+
 			if(!o.booleanValue() && n.booleanValue()) {
-				if(enabled)
-					voice.talk("Landed");
+				voice.talk("Landed");
 			}
 		});
-		
-	
+
+
 		// report battery status every 30 seconds if armed and below 60%
 		wq.addCyclicTask("LP", 30000, () -> {
 			if(!properties.getArmedProperty().get())
 				return;
 			float v = (float)model.getValue("BATP")*100f;
-			if(v > 60.0f)
-				return;
-			String s = String.format("Battery is at %.0f percent.",v);
-			System.out.println(s);
-			if(enabled)
-				voice.talk(s);
+			if(v < BATTERY_CAPACITY_LIMT)
+				voice.talk(String.format("Battery is at %.0f percent.",v));
 		});
-		
+
 		// report altitude every 45 seconds
 		wq.addCyclicTask("LP", 45000, () -> {
 			if(!properties.getArmedProperty().get() || properties.getLandedProperty().get())
 				return;
-			String s = String.format("Relative altitude is %.1f meters.",model.getValue("ALTRE"));
-			System.out.println(s);
-			if(enabled)
-				voice.talk(s);
+			voice.talk(String.format("Relative altitude is %.1f meters.",model.getValue("ALTRE")));
 		});
 
 
