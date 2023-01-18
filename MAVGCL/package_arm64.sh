2f273815da7d703232709920811950ebe2e82416
ecmnet <ecmnet@laptop-von-eike.fritz.box>
2023-01-18 07:03:25 +0100

    Enable OSX x86_64 and arm64 build

---
+++ MAVGCL/package_arm64.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.se,jdk.httpserver,javafx.controls,javafx.fxml --output ./target/custom_jre --module-path ./jmods/arm64
+
+jpackage --input target/deploy/ --name MAVGAnalysis --main-jar MAVGCL-0.8.0.jar --main-class com.comino.flight.MainApp --type dmg --icon target/deploy/MAVGAnalysis.icns --runtime-image ./target/custom_jre --java-options '-Djava.library.path=./native --add-opens javafx.base/com.sun.javafx.collections=ALL-UNNAMED  --add-opens javafx.base/com.sun.javafx.runtime=ALL-UNNAMED '
