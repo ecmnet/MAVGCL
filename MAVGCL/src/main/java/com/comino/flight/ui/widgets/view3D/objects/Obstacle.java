bebc34ed57275e92906c5f309543683b97f2da0f
ecmnet <ecmnet@laptop-von-eike.fritz.box>
2023-03-04 13:40:36 +0100

    Box instead of sphere, Intersection fixed

--- MAVGCL/src/main/java/com/comino/flight/ui/widgets/view3D/objects/Obstacle.java
+++ MAVGCL/src/main/java/com/comino/flight/ui/widgets/view3D/objects/Obstacle.java
@@ -14,13 +14,12 @@ import javafx.scene.shape.Sphere;
 
 public class Obstacle extends Xform {
 	
-	private final Sphere        obstacle = new Sphere(20);
-	private final Sphere        boundary = new Sphere(30);
+	private final Box           obstacle = new Box();
 	private final VehicleModel  vehicle;
 	private final PhongMaterial boundary_no_collision = new PhongMaterial();
 	private final PhongMaterial boundary_collision    = new PhongMaterial();
 	private final Point3D_F64   o = new Point3D_F64();
-	private final Point3D_F64   v = new Point3D_F64();
+	private final Point3D_F64   s = new Point3D_F64();
 	
 	private boolean show = false;
 	
@@ -29,39 +28,33 @@ public class Obstacle extends Xform {
 		
 		this.vehicle = vehicle;
 		
-		PhongMaterial material1 = new PhongMaterial();
-		material1.setDiffuseColor(Color.web("rgba( 190,190,190,1.0 )"));
-		obstacle.setMaterial(material1);
+		boundary_no_collision.setDiffuseColor(Color.web("rgba( 50,50,50,0.9 )"));
+		boundary_collision.setDiffuseColor   (Color.web("rgba( 50,10,10,0.9 )"));
+		obstacle.setMaterial(boundary_no_collision);
 		
-		boundary_no_collision.setDiffuseColor(Color.web("rgba( 10,10,10,0.1 )"));
-		boundary_collision.setDiffuseColor   (Color.web("rgba( 50,10,10,0.1 )"));
-		boundary.setMaterial(boundary_no_collision);
-		
-		this.getChildren().addAll(obstacle,boundary);
+		this.getChildren().addAll(obstacle);
 		
 	}
 	
 	public void updateState(AnalysisDataModel model, float offset) {
 	
-		
 		o.setTo(-model.getValue("SLAMOBY")*100f, (-model.getValue("SLAMOBZ")-offset ) * 100f, model.getValue("SLAMOBX")*100f);
+		s.setTo( model.getValue("SLAMSOBY")*100f, (model.getValue("SLAMSOBZ")) * 100f, model.getValue("SLAMSOBX")*100f);
 		
 		if(show && MSP3DUtils.isFinite(o) && o.normSq()>0) {
 			
 			this.setTranslate(o.x,o.y+10,o.z);
-			v.setTo(vehicle.getTranslateX(),vehicle.getTranslateY(),vehicle.getTranslateZ()); v.scale(-1f);
-			v.plusIP(o);
-//			
-//			if(vehicle.intersects(obstacle.getBoundsInLocal())) {
-//				boundary.setMaterial(boundary_collision);
-//			} else {
-//				boundary.setMaterial(boundary_no_collision);
-//			}
-//			
-			if(v.norm() < boundary.getRadius())
-				boundary.setMaterial(boundary_collision);
-			else
-				boundary.setMaterial(boundary_no_collision);
+			this.obstacle.setWidth(s.x);
+			this.obstacle.setHeight(s.y);
+			this.obstacle.setDepth(s.z);
+			
+			
+			if(this.getBoundsInParent().intersects(vehicle.getBoundsInParent())) {
+				obstacle.setMaterial(boundary_collision);
+			} else {
+				obstacle.setMaterial(boundary_no_collision);
+			}
+
 			this.setVisible(true);
 		} else
 			this.setVisible(false);
