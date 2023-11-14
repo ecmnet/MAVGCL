1d4d1f866eea64eed368398183e8b314d4f2ad08
ecmnet <ecmnet@laptop-von-eike.fritz.box>
2023-11-14 09:08:57 +0100

    Event argument parsing (WIP)

--- MAVGCL/src/main/java/com/comino/flight/events/MAVEventMataData.java
+++ MAVGCL/src/main/java/com/comino/flight/events/MAVEventMataData.java
@@ -14,13 +14,14 @@ import com.google.gson.JsonParser;
 
 
 public class MAVEventMataData {
-	
-	
+
+
 	private static MAVEventMataData instance;
 
-	private Map<String, List<String>>   enum_map = new HashMap<String, List<String>>( );
+	private Map<String, Map<Integer,String>>   enum_map = new HashMap<String, Map<Integer,String>>( );
 	private Map<Integer, EventMetaData> event_map = new HashMap<Integer,EventMetaData>( );
-	
+	private String namespace;
+
 	public static MAVEventMataData getInstance( ) {
 		if(instance==null)
 			instance = new MAVEventMataData("all_events.json");
@@ -28,19 +29,20 @@ public class MAVEventMataData {
 	}
 
 	private MAVEventMataData(String filename ) {
-		
-		System.out.println("Event parsing: "+filename);
-	
+
 		JsonElement root = JsonParser.parseReader(new InputStreamReader(getClass().getResourceAsStream(filename)));
+
+		JsonElement name_space = root.getAsJsonObject().get("components").getAsJsonObject().get("1").getAsJsonObject().get("namespace");
+		namespace = name_space.getAsString();
 
 		// Get enums from defintion
 		JsonElement enums = root.getAsJsonObject().get("components").getAsJsonObject().get("1").getAsJsonObject().get("enums");
 		enums.getAsJsonObject().entrySet().forEach((enum_entry) -> {
-			final List<String> entries = new ArrayList<>();
+			final HashMap<Integer,String> entries = new HashMap<>();
 			enum_entry.getValue().getAsJsonObject().get("entries").getAsJsonObject().entrySet().forEach((enum_value)-> {
-				entries.add(enum_value.getValue().getAsJsonObject().get("description").getAsString());
+				entries.put(Integer.parseInt(enum_value.getKey()),enum_value.getValue().getAsJsonObject().get("description").getAsString());
 			});
-			enum_map.put(enum_entry.getKey(), entries);
+			enum_map.put(namespace+"::"+enum_entry.getKey(), entries);
 		});
 
 		// Get events from definition
@@ -51,68 +53,76 @@ public class MAVEventMataData {
 				event_meta_data.message = event.getValue().getAsJsonObject().get("message").getAsString();
 				var _arguments = event.getValue().getAsJsonObject().get("arguments");
 				if(_arguments!=null) {
-				  _arguments.getAsJsonArray().forEach((argument) -> {
-					Argument arg = new Argument();
-					arg.name = argument.getAsJsonObject().get("name").getAsString();
-					arg.type = argument.getAsJsonObject().get("type").getAsString();
-					event_meta_data.arguments.add(arg);
-				});
+					_arguments.getAsJsonArray().forEach((argument) -> {
+						Argument arg = new Argument();
+						arg.name = argument.getAsJsonObject().get("name").getAsString();
+						arg.type = argument.getAsJsonObject().get("type").getAsString();
+						event_meta_data.arguments.add(arg);
+					});
 				}
 				event_map.put(Integer.parseInt(event.getKey()),event_meta_data);
 			});
 		});
-		
-		System.out.println("Event metadata parsed");
-		
-//		event_map.forEach((id,event)->{
-//			System.out.println(id+": "+event.message);
-//			event.arguments.forEach((arg)-> {
-//				System.out.println(" -> "+arg.name+": "+arg.type);
-//			});
-//		});
-		
-//		enum_map.forEach((k,v)-> {
-//			System.out.println(k);
-//			v.forEach((e)-> {
-//				System.out.println(" --> "+e);
-//			});
-//		});
-		
+
+
+		System.out.println("Event metadata parsed for namespace: "+namespace);
+
+		//		event_map.forEach((id,event)->{
+		//			System.out.println(id+": "+event.message);
+		//			event.arguments.forEach((arg)-> {
+		//				System.out.println(" -> "+arg.name+": "+arg.type);
+		//			});
+		//		});
+
+		//		enum_map.forEach((k,v)-> {
+		//			System.out.println(k);
+		//			v.forEach((e,l)-> {
+		//				System.out.println(" --> "+e+":"+l);
+		//			});
+		//		});
+
 	}
-	
+
 	public String buildMessageFromMAVLink( msg_event event_msg) {
 		EventMetaData emd = this.event_map.get((int)(event_msg.id & 0x0FFFFFF));
 		if(emd == null)
 			return "[px4] Event "+(int)(event_msg.id & 0x0FFFFFF)+" received";
 		return "[px4] "+emd.buildMessage(event_msg);
 	}
-	
+
 	private class EventMetaData {
-		
+
 		public String message;
 		public List<Argument> arguments = new ArrayList<Argument>();;
-		
+
 		public String buildMessage(msg_event event_msg) {
 			String msg = this.message;
-			
 			for(int i=0; i<arguments.size();i++) {
-//				// TODO Namespace in types (Offset 5=PX4::)
-//				if(enum_map.containsKey(arguments.get(i).type.substring(5)))
-//					msg = msg.replace("{"+(i+1)+"}", enum_map.get(arguments.get(i).type.substring(5)).get(event_msg.arguments[i]));
-//				else	
-				// TODO: Different types of arguments
-				msg = msg.replace("{"+(i+1)+"}", "");
+				if(enum_map.containsKey(arguments.get(i).type))
+					msg = msg.replace("{"+(i+1)+"}", enum_map.get(arguments.get(i).type).get(event_msg.arguments[i]));
+				else	{
+					switch(arguments.get(i).type) {
+					case "uint8_t":
+						msg = msg.replace("{"+(i+1)+"}", String.valueOf(event_msg.arguments[i] & 0xFF));
+					case "uint16_t":
+						msg = msg.replace("{"+(i+1)+"}", String.valueOf(event_msg.arguments[i] & 0xFFFF));
+					default:
+						msg = msg.substring(0,msg.indexOf('{'));
+					}
+				}
 			}
 			return msg;
 		}
 	}
-	
+
 	private class Argument {
 		public String name;
 		public String type;
 	}
 
-	
+
+
+
 
 
 
---
+++ MAVGCL/src/main/java/com/comino/flight/events/MAVEventMetaDataTest.java
@@ -0,0 +1,10 @@
+package com.comino.flight.events;
+
+public class MAVEventMetaDataTest {
+
+	public static void main(String[] args) {
+		MAVEventMataData m = MAVEventMataData.getInstance();
+
+	}
+
+}
